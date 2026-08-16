b89dab0dd2c2fdf2f70ee1b5a55273f3597c3f14
Walter Larrea <walterlarrea@live.com>
2026-08-16 00:14:05 -0300

    setup inicial con swing

---
+++ edEXT-2026-grupo10/src/main/java/com/grupo10/edext/grupo10/EdEXT2026Grupo10.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupo10.edext.grupo10;
+
+/**
+ *
+ * @author Walter
+ */
+public class EdEXT2026Grupo10 {
+
+    public static void main(String[] args) {
+        // Esto no lo estamos usando, ver "mainClass" en el pom.xml
+        System.out.println("Hello World!");
+    }
+}
