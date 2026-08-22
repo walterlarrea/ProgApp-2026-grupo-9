978137c538948a9fd84f2912e1db2e36802b5d6e
MateGamer12 <mateomdl2007@gmail.com>
2026-08-22 14:27:04 -0300

    Commit

---
+++ edEXT-2026-grupo9/src/main/java/com/grupo9/edext/grupo9/servidor_central/controller/usuario/Usuario.java
@@ -0,0 +1,17 @@
+package com.grupo9.edext.grupo9.servidor_central.controller.usuario;
+
+import com.grupo9.edext.grupo9.servidor_central.dominio.DataUsuario;
+import com.grupo9.edext.grupo9.servidor_central.dominio.DataDocente;
+import java.time.LocalDate;
+import java.util.Set;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+/**
+ *
+ * @author Usuario
+ */
+public class Usuario {
+    
+}
